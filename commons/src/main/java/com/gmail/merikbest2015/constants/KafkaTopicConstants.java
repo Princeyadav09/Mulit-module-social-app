f386bfb61959303b1b73a5abfd5b34c54631282b
[princeyadav09] <[princekumaryadav09@gmail.com]>
2024-03-14 20:30:14 +0530

    commit

---
+++ commons/src/main/java/com/gmail/merikbest2015/constants/KafkaTopicConstants.java
@@ -0,0 +1,8 @@
+package com.gmail.merikbest2015.constants;
+
+public class KafkaTopicConstants {
+    public static final String UPDATE_USER_TOPIC = "user-service.user.update";
+    public static final String BLOCK_USER_TOPIC = "user-service.user.block";
+    public static final String FOLLOW_USER_TOPIC = "user-service.user.follow";
+    public static final String FOLLOW_REQUEST_USER_TOPIC = "user-service.user.follow-request";
+}
---
+++ tweet-service/src/main/java/com/gmail/merikbest2015/service/UserService.java
@@ -0,0 +1,28 @@
+package com.gmail.merikbest2015.service;
+
+import com.gmail.merikbest2015.model.Tweet;
+import com.gmail.merikbest2015.model.User;
+import com.gmail.merikbest2015.repository.projection.UserProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface UserService {
+
+    User getAuthUser();
+
+    User getUserById(Long userId);
+
+    Long getUserIdByUsername(String username);
+
+    Boolean isUserExists(Long userId);
+
+    boolean isUserHavePrivateProfile(Long userId);
+
+    boolean isMyProfileBlockedByUser(Long userId);
+
+    Page<UserProjection> getLikedUsersByTweet(Tweet tweet, Pageable pageable);
+
+    Page<UserProjection> getRetweetedUsersByTweet(Tweet tweet, Pageable pageable);
+
+    Page<UserProjection> getTaggedImageUsers(Tweet tweet, Pageable pageable);
+}
